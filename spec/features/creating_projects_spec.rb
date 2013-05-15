7f233ebd6573318e00225caafcee40dd2534cfea
Douglas Manzelmann <dougamanzelmann@gmail.com>
2013-05-15 16:03:21 -0400

    Add validation to ensure names are specific when creating projects

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do 
-  scenario "Can create a project" do
+  before do 
     visit '/'
-
     click_link 'New Project'
+
+  end
+
+
+  scenario "can create a project" do
     fill_in 'Name', with: 'Sublime Text 2'
     fill_in 'Description', with: 'A text editor for all OSs'
     click_button 'Create Project'
@@ -16,5 +20,12 @@ feature 'Creating Projects' do
 
     title = "Sublime Text 2 - Projects - Ticketee"
     expect(find("title").native.text).to have_content(title)
+  end
+
+  scenario "cannot create a project without a name" do 
+    click_button 'Create Project'
+
+    expect(page).to have_content("Project has not been created.")
+    expect(page).to have_content("Name can't be blank")
   end
 end
