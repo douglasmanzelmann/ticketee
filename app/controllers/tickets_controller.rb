26491f40a592d00c4bd6a6a786300ece103055fd
Douglas Manzelmann <dougamanzelmann@gmail.com>
2013-06-14 08:16:31 -0700

    On page 275

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,6 @@
 class TicketsController < ApplicationController
 
-  before_action :require_signin!, except: [:show, :index]
+  before_action :require_signin!
   before_action :set_project
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
