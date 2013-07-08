0ba8c6c70dc8627d4b7a90c052e126748ce55e5c
Douglas Manzelmann <dougamanzelmann@gmail.com>
2013-07-08 16:51:05 -0400

    bottom of page 332

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -46,7 +46,10 @@ feature "Creating Tickets" do
   scenario "Creating a ticket with an attachment" do 
     fill_in "Title", with: "Add documentation for blink tag"
     fill_in "Description", with: "The blink tag has a speed attribute"
-    attach_file "File", "spec/fixtures/speed.txt"
+    
+    attach_file "File #1", "spec/fixtures/speed.txt"
+    attach_file "File #2", "spec/fixtures/spin.txt"
+    attach_file "File #3", "spec/fixtures/gradient.txt"
     click_button "Create Ticket"
 
     expect(page).to have_content("Ticket has been created")
